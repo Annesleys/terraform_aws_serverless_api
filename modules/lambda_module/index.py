50608514f2b7b1c55f3a01b2e41c547e58efa930
Annesley@codecommit-at-789734661575 <Annesley@codecommit-at-789734661575>
2023-12-01 01:16:05 +0530

    fixed lint errors

--- modules/lambda_module/index.py
+++ modules/lambda_module/index.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Origin': '*'
         },
         'body': json.dumps({
-            'status': "OK"
+            'status': 'OK'
         }),
         "isBase64Encoded": False
     }
