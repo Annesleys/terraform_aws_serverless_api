dea6551efaf19d6d0e3209ca3ee9a5b20c806cd2
Annesley@codecommit-at-789734661575 <Annesley@codecommit-at-789734661575>
2023-12-01 00:20:35 +0530

    added depends on for null resource

--- modules/lambda_module/lambda.tf
+++ modules/lambda_module/lambda.tf
@@ -70,5 +70,5 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   role          = aws_iam_role.lambda_role.arn
   handler       = "${var.artifact_name}.lambda_handler"
   runtime       = "python3.8"
-  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role, null_resource.dummy_artifact]
 }
