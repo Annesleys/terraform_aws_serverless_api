37e9c7b97a7096dab9722946729c56af08ad59c6
Annesley@codecommit-at-789734661575 <Annesley@codecommit-at-789734661575>
2023-12-01 09:54:01 +0530

    fixed lambda role policy name

--- modules/lambda_module/lambda.tf
+++ modules/lambda_module/lambda.tf
@@ -18,7 +18,7 @@ EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
-  name        = "aws_iam_policy_for_terraform_aws_lambda_role"
+  name        = "aws_iam_policy_for_terraform_aws_lambda_role${var.env}"
   path        = "/"
   description = "AWS IAM Policy for managing aws lambda role"
   policy      = <<EOF
